9bb112fdb399214dd07ad71e98cb42642ed4807e
HEMMOUDA Aymane <ahemmouda@gmail.com>
2023-09-03 19:56:04 +0100

    version 0.2

--- Joojle/src/joojle/Joojle.java
+++ Joojle/src/joojle/Joojle.java
@@ -10,10 +10,14 @@ public class Joojle {
 	public static void main(String[] args) throws Exception {
 		String jarFilePath;
 		String query;
+		int MAX = 50;
 		
 		if (args.length >= 2) {
 			jarFilePath = args[0];
 			query = args[1];
+			if (args.length >= 3) {
+				MAX = Integer.parseInt(args[2]);
+			}
 		} else {
 			jarFilePath = ""; // Put the link to you jar file here if you are not using the args
 			query = ""; // and the query here
@@ -22,7 +26,6 @@ public class Joojle {
         Loader.loadFrom(jarFilePath);
         System.out.println(String.format("Loaded %d functions from `%s`", Loader.LOADED_FUNCTIONS.size(), jarFilePath));
         
-        final int MAX = 50;
         int count = 1;
         System.out.println(String.format("Showing top %d results:\n", MAX));
         for (Result result : Searcher.search(query, Loader.LOADED_FUNCTIONS)) {
