13d705381d9822a4e419550cb45a0014fd3cefaa
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-18 11:17:10 +0200

    Create WindowPane.java

---
+++ src/main/java/hemmouda/joojle/gui/WindowPane.java
@@ -0,0 +1,32 @@
+package hemmouda.joojle.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * TODO mention that it could have been done with reseting the objects or reusing them
+ * but whatever, not that much of memory use
+ * also say that the pane communicates with the window to tell it
+ * what to do next
+ */
+public abstract class WindowPane extends JPanel {
+
+    protected Window parent;
+
+    public WindowPane (Window parent) {
+        super();
+
+        this.parent = parent;
+    }
+
+    /**
+     * @return the size the parent should use.
+     */
+    public abstract Dimension getPreferredWindowSize ();
+
+    /**
+     * @return the title the parent should use.
+     */
+    public abstract String getPreferredTitle ();
+
+}
