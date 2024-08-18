3b7ba53a709e3b2b10921c2a922f749246214a33
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-18 11:45:56 +0200

    Create SearchPane.java

---
+++ src/main/java/hemmouda/joojle/gui/SearchPane.java
@@ -0,0 +1,51 @@
+package hemmouda.joojle.gui;
+
+import hemmouda.joojle.api.core.MethodRecord;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+/**
+ * The main pane that lets the user search
+ * and see results
+ */
+public class SearchPane extends WindowPane {
+
+    /**
+     * The loaded methods on which the search
+     * and filtering will be preformed
+     */
+    private List<MethodRecord> loadedMethods;
+    /**
+     * The path of the JAR file that has been loaded.
+     * Only used to display information
+     */
+    private String jarFilePath;
+
+    public SearchPane(Window parent, String jarFilePath, List <MethodRecord> loadedMethods) {
+        super(parent);
+
+        this.jarFilePath = jarFilePath;
+        this.loadedMethods = loadedMethods;
+
+        init();
+    }
+
+    /**
+     * This boi thicc.
+     */
+    private void init () {
+
+    }
+
+    @Override
+    public Dimension getPreferredWindowSize() {
+        return new Dimension(800, 600);
+    }
+
+    @Override
+    public String getPreferredTitle() {
+        return jarFilePath;
+    }
+}
