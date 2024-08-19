49d24f8f9b7a8586c14abb4e3a42217cbedc88f6
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-19 20:16:59 +0200

    Create MethodType.java

---
+++ src/main/java/hemmouda/joojle/api/core/MethodType.java
@@ -0,0 +1,13 @@
+package hemmouda.joojle.api.core;
+
+/**
+ * The type of the method, is
+ * it an actual method
+ * or a constructor.
+ */
+public enum MethodType {
+
+    METHOD,
+    CONSTRUCTOR;
+
+}
