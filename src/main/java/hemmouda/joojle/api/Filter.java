b25c010ad3207d4d7e1fe8cf6231fe72fd3f9b13
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-19 20:17:26 +0200

    Create Filter.java
    It's done i guess?

---
+++ src/main/java/hemmouda/joojle/api/Filter.java
@@ -0,0 +1,51 @@
+package hemmouda.joojle.api;
+
+import hemmouda.joojle.api.core.MethodRecord;
+import hemmouda.joojle.api.core.MethodScope;
+import hemmouda.joojle.api.core.MethodType;
+import hemmouda.joojle.api.core.MethodVisibility;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A utility class that filters
+ * the methods according
+ * to
+ * {@link MethodType},
+ * {@link MethodVisibility}, and
+ * {@link MethodScope}.
+ */
+public class Filter {
+
+    /**
+     * Filters and return a new list
+     * that only contain the methods
+     * that match the given "filters".
+     * If a "filter" is <code>null</code>
+     * it's skipped.
+     */
+    public List<MethodRecord> filter (
+            List<MethodRecord> list,
+            MethodType type,
+            MethodVisibility visibility,
+            MethodScope scope) {
+
+        // Make a new copy. That is a LinkedList because
+        // we will be doing a lot of modifications
+        var filtered = new LinkedList<>(list);
+
+        if (type != null) {
+            filtered.removeIf(method -> method.getType() != type);
+        }
+        if (visibility != null) {
+            filtered.removeIf(method -> method.getVisibility() != visibility);
+        }
+        if (scope != null) {
+            filtered.removeIf(method -> method.getScope() != scope);
+        }
+
+        return filtered;
+    }
+
+}
