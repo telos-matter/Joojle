e61d7383a35c8702ec3a03fa56a28aa113ae04c4
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-19 20:17:02 +0200

    Create MethodVisibility.java

---
+++ src/main/java/hemmouda/joojle/api/core/MethodVisibility.java
@@ -0,0 +1,33 @@
+package hemmouda.joojle.api.core;
+
+import java.lang.reflect.Executable;
+import java.lang.reflect.Modifier;
+
+/**
+ * The visibility of the method.
+ */
+public enum MethodVisibility {
+
+    PUBLIC,
+    PRIVATE,
+    PROTECTED;
+
+    /**
+     * @return the given executable visibility.
+     * Or <code>null</code> if none match.
+     */
+    public static MethodVisibility getVisibility (Executable executable) {
+        var mod = executable.getModifiers();
+        
+        if (Modifier.isPublic(mod)) {
+            return PUBLIC;
+        } else if (Modifier.isPrivate(mod)) {
+            return PRIVATE;
+        } else if (Modifier.isProtected(mod)) {
+            return PROTECTED;
+        } else {
+            return null;
+        }
+    }
+
+}
