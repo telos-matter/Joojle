9127376a53cbe7d11c983c6f0f80f70518548973
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-20 13:11:05 +0200

    Create Icons.java

---
+++ src/main/java/hemmouda/joojle/gui/icons/Icons.java
@@ -0,0 +1,42 @@
+package hemmouda.joojle.gui.icons;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+/**
+ * Loads the icons from the resources
+ */
+public class Icons {
+
+    public static final ImageIcon RETURN_ICON;
+    public static final ImageIcon HELP_ICON;
+
+    static {
+        RETURN_ICON = loadImageIcon("return.png", 25, 25);
+        HELP_ICON = loadImageIcon("help.png", 25, 25);
+    }
+
+    private static ImageIcon loadImageIcon (String name, int width, int height) {
+        var image = loadImage(name);
+        var scaled = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaled);
+    }
+
+    private static BufferedImage loadImage (String name) {
+        String path = "/hemmouda/joojle/" + name;
+        InputStream imageStream = Icons.class.getResourceAsStream(path);
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(imageStream);
+        } catch (Exception e) {
+            System.err.println("Couldn't load this image: " + name);
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+}
