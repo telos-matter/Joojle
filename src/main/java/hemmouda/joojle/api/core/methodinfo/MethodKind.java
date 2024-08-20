f411100813e962935827a9d12acbacdb58bb4280
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-20 12:03:23 +0200

    Refactor MethodType -> MethodKind

--- src/main/java/hemmouda/joojle/api/core/methodinfo/MethodKind.java
+++ src/main/java/hemmouda/joojle/api/core/methodinfo/MethodKind.java
@@ -1,11 +1,11 @@
 package hemmouda.joojle.api.core.methodinfo;
 
 /**
- * The type of the method, is
+ * The kind of the method, is
  * it an actual method
  * or a constructor.
  */
-public enum MethodType {
+public enum MethodKind {
 
     METHOD,
     CONSTRUCTOR;
