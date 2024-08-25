d60bb38d9cb46117228fef616f7f462e3d0bf718
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-25 19:14:26 +0200

    Create Consts.java

---
+++ src/main/java/hemmouda/joojle/gui/panes/searchpane/util/Consts.java
@@ -0,0 +1,34 @@
+package hemmouda.joojle.gui.panes.searchpane.util;
+
+import hemmouda.joojle.api.core.methodinfo.MethodKind;
+import hemmouda.joojle.api.core.methodinfo.MethodScope;
+import hemmouda.joojle.api.core.methodinfo.MethodVisibility;
+
+import java.awt.*;
+import java.util.Map;
+
+/**
+ * Groups a bunch of constants that are used by
+ * {@link hemmouda.joojle.gui.panes.searchpane.SearchPane}
+ */
+public class Consts {
+
+    public static final int FONT_SIZE = 14;
+
+    public static final Map<MethodKind, Color> METHOD_KIND_COLOR = Map.of(
+            MethodKind.METHOD, new Color(10, 10, 230),
+            MethodKind.CONSTRUCTOR, new Color(255, 140, 0)
+    );
+
+    public static final Map<MethodVisibility, Color> METHOD_VISIBILITY_COLOR = Map.of(
+            MethodVisibility.PUBLIC, Color.green,
+            MethodVisibility.PRIVATE, Color.red,
+            MethodVisibility.PROTECTED, Color.orange
+    );
+
+    public static final Map<MethodScope, Color> METHOD_SCOPE_COLOR = Map.of(
+            MethodScope.INSTANCE, new Color(0, 128, 128),
+            MethodScope.STATIC, new Color(128, 0, 128)
+    );
+
+}
