16ef6252caf3d7511b8203cc93aab9dc2266618d
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-29 11:53:50 +0200

    Update App.java
    Moved UILaF here
    + ToolTip dismiss thing

--- src/main/java/hemmouda/joojle/App.java
+++ src/main/java/hemmouda/joojle/App.java
@@ -7,6 +7,14 @@ import javax.swing.*;
 public interface App {
 
     public static void main(String[] args)  {
+        // Set the look and feel to System if possible
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+        // And then set tooltip dismiss delay
+        ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
+
+        // Start
         SwingUtilities.invokeLater(() -> {
             Window window = new Window();
             window.showSelectionWindow();
